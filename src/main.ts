2df64bfb631d0f70352b56617b538eff214bb347
Leon Coto <leoncoto@gmail.com>
2019-05-30 18:46:49 +0100

    add logic functions, tests and other stuff ...

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,26 @@
-type Nat = Zero | Succ;
+import assert from "assert";
 
-interface Zero {
+export type Nat = Zero | Succ;
+
+export interface Zero {
   k: "Zero";
 }
 
-interface Succ {
+export interface Succ {
   k: "Succ";
   v: Nat;
 }
-const succ = (v: Nat): Nat => ({ k: "Succ", v });
-const zero: Nat = { k: "Zero" };
 
-const one = succ(zero);
-const two = succ(one);
-const three = succ(two);
-const four = succ(three);
-const five = succ(four);
+export const succ = (v: Nat): Nat => ({ k: "Succ", v });
 
-const plus = (x: Nat, y: Nat): Nat => {
-  if (x.k === "Zero") {
-    return y;
-  } else if (y.k === "Zero") {
-    return x;
-  } else {
-    return { k: "Succ", v: plus(x.v, y) };
-  }
-};
+export const zero: Nat = { k: "Zero" };
+export const one = succ(zero);
+export const two = succ(one);
+export const three = succ(two);
+export const four = succ(three);
+export const five = succ(four);
 
-const plus_ = (x: Nat) => (y: Nat) => plus(x, y);
-
-const times = (x: Nat, y: Nat): Nat => {
-  if (x.k === "Zero" || y.k === "Zero") {
-    return zero;
-  } else {
-    return {
-      k: "Succ",
-      // v: plus(times(one, y.v), times(x.v, y))
-      v: plus(y.v, times(x.v, y))
-    };
-  }
-};
-
-// cart :: [a] -> [b] -> [(a,b)]
-// cart [] _ = []
-// cart _ [] = []
-// cart (x:xs) (y:ys) = (x,y) : (cart [x] ys) `conc` (cart xs (y:ys))
-
-const plus1 = plus_(succ(zero));
-
-const toInt = (x: Nat): number => {
+export const toInt = (x: Nat): number => {
   switch (x.k) {
     case "Zero":
       return 0;
@@ -59,16 +31,134 @@ const toInt = (x: Nat): number => {
   }
 };
 
-const show = (x: Nat) => {
-  console.log(JSON.stringify(x, null, 2));
-  console.log(toInt(x));
+export const fromNumber = (x: number): Nat => {
+  assert.equal(x, Math.floor(x), `Error: "${x}" is not an integer`);
+  assert(0 <= x, `Error: "${x}" is not positive`);
+  return [...Array(x)].reduce(succ, zero);
 };
+
+export const plus = (x: Nat, y: Nat): Nat => {
+  if (x.k === "Zero") {
+    return y;
+  } else if (y.k === "Zero") {
+    return x;
+  } else {
+    return { k: "Succ", v: plus(x.v, y) };
+  }
+};
+
+export const times = (x: Nat, y: Nat): Nat => {
+  if (x.k === "Zero" || y.k === "Zero") {
+    return zero;
+  } else {
+    return {
+      k: "Succ",
+      v: plus(y.v, times(x.v, y))
+    };
+  }
+};
+
+export const isLessThan = (x: Nat, y: Nat): boolean => {
+  if (x.k === "Zero" && y.k === "Zero") {
+    return false;
+  } else if (x.k === "Zero" && y.k === "Succ") {
+    return true;
+  } else if (x.k === "Succ" && y.k === "Zero") {
+    return false;
+  } else {
+    return isLessThan((<Succ>x).v, (<Succ>y).v);
+  }
+};
+
+export const isGreaterThan = (x: Nat, y: Nat): boolean => {
+  if (x.k === "Zero" && y.k === "Zero") {
+    return false;
+  } else if (x.k === "Zero" && y.k === "Succ") {
+    return false;
+  } else if (x.k === "Succ" && y.k === "Zero") {
+    return true;
+  } else {
+    return isGreaterThan((<Succ>x).v, (<Succ>y).v);
+  }
+};
+
+export const isEqualTo = (x: Nat, y: Nat): boolean => {
+  if (x.k === "Zero" && y.k === "Zero") {
+    return true;
+  } else if (x.k === "Succ" && y.k === "Zero") {
+    return false;
+  } else if (x.k === "Zero" && y.k === "Succ") {
+    return false;
+  } else {
+    return isEqualTo((<Succ>x).v, (<Succ>y).v);
+  }
+};
+
+export const subtract = (x: Nat, y: Nat): Nat => {
+  if (x.k === "Zero" && y.k === "Zero") {
+    return x;
+  } else if (x.k === "Zero" && y.k === "Succ") {
+    throw new Error(`Error: subtract(x, y) "y" must be less than or equal to "x"`);
+  } else if (x.k === "Succ" && y.k === "Zero") {
+    return x;
+  } else {
+    return subtract((<Succ>x).v, (<Succ>y).v);
+  }
+};
+
+export const toString = (x: Nat): String => {
+  switch (x.k) {
+    case "Zero":
+      return "Zero";
+    case "Succ":
+      return `Succ ( ${toString(x.v)} )`;
+    default:
+      return assertNever(x);
+  }
+};
+
+assert.throws(() => subtract(zero, one), "subtract(x, y) should fail when x is less than y");
+assert.throws(() => fromNumber(1.1), "fromNumber(x) should fail for non integer values");
+assert.throws(() => fromNumber(-1), "fromNumber(x) should fail for negative values");
+
+assert(isEqualTo(zero, zero), "isEqualTo(zero, zero) should be true");
+assert(!isEqualTo(zero, one), "isEqualTo(zero, one) should be false");
+assert(!isEqualTo(one, zero), "isEqualTo(one, zero) should be false");
+
+assert(!isLessThan(zero, zero), "isLessThan(zero, zero) should be false");
+assert(isLessThan(zero, one), "isLessThan(zero, one) should be true");
+assert(!isLessThan(one, zero), "isLessThan(one, zero) should be false");
+
+assert(!isGreaterThan(zero, zero), "isGreaterThan(zero, zero) should be false");
+assert(!isGreaterThan(zero, one), "isGreaterThan(zero, one) should be false");
+assert(isGreaterThan(one, zero), "isGreaterThan(one, zero)  should be true");
+
+assert(isEqualTo(zero, plus(zero, zero)), "plus(zero, zero) should be zero");
+assert(isEqualTo(one, plus(zero, one)), "plus(zero, one) should be one");
+assert(isEqualTo(one, plus(one, zero)), "plus(one, zero) should be one");
+assert(isEqualTo(two, plus(one, one)), "plus(one, one) should be two");
+
+assert(isEqualTo(zero, times(zero, zero)), "times(zero, zero) should be zero");
+assert(isEqualTo(zero, times(zero, one)), "times(zero, one) should be zero");
+assert(isEqualTo(zero, times(one, zero)), "times(one, zero) should be zero");
+assert(isEqualTo(one, times(one, one)), "times(one, one) should be one");
+assert(isEqualTo(two, times(one, two)), "times(one, two) should be two");
+assert(isEqualTo(two, times(two, one)), "times(two, one) should be two");
+
+console.log(toString(zero));
+console.log(toString(one));
+console.log(toString(two));
+console.log(toString(three));
+console.log(toString(four));
+console.log(toString(five));
+
+const plusFive = (x: Nat) => plus(five, x);
+console.log(toString(plusFive(one)));
+console.log(toString(plusFive(two)));
+console.log(toString(plusFive(three)));
+console.log(toString(plusFive(four)));
+console.log(toString(plusFive(five)));
 
 function assertNever(x: never): never {
   throw new Error("Unexpected object: " + x);
 }
-
-// show(plus(zero, succ(zero)));
-// show(plus1(zero));
-// show(plus(succ(zero), succ(zero)));
-show(times(three, five));
